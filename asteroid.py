c64434d79a1cd8affcb98819d4376d82a3baaae4
Piotr Łepkowski <piotr.l@bitnoi.se>
2024-09-16 22:16:34 +0200

    feat: add asteroids

---
+++ asteroid.py
@@ -0,0 +1,14 @@
+
+import pygame
+from circleshape import CircleShape
+
+
+class Asteroid (CircleShape):
+  def __init(self, x, y, radius):
+    pass
+
+  def draw(self, screen):
+    pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+  def update(self, dt):
+    self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+from asteroid import Asteroid
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
 
 def main():
   print("Starting asteroids!")
@@ -17,8 +19,15 @@ def main():
 
   updatable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
 
   Player.containers = (updatable, drawable)
+
+  Asteroid.containers = (asteroids, updatable, drawable)
+
+  AsteroidField.containers = (updatable)
+
+  asteroid_field = AsteroidField()
 
   player = Player(x, y, PLAYER_RADIUS)
 
